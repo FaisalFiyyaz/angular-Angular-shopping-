5042ed8301984dfd6c14848dfd7bcfc60a2f01d1
FaisalFiyyaz <faisalfiyyaz@gmail.com>
2019-09-17 14:39:43 +1000

    Shoping cart complete

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -24,6 +24,6 @@ export class ProductDetailsComponent implements OnInit {
 
   addToCart(product) {
     window.alert('Your product has been added to the cart');
-    this.cartService.addToCart(products);
+    this.cartService.addToCart(product);
   }
 }
